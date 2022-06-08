67e237bb635e1c2a8e5eb4860eda033d4aa53093
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2022-06-08 20:24:52 -0300

    entrega editada

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const mongoDB = process.env.MONGO || "mongodb://localhost/testingPassport";
+const mongoDB_URI = process.env.MONGO || "mongodb://localhost/testingPassport";
 (async () => {
     try {
-        const db = await mongoose.connect(mongoDB)
+        const db = await mongoose.connect(mongoDB_URI)
         console.log('Conectado a mongoDB ' + db.connection.host + ':' + db.connection.port + '/' + db.connection.name);
     } catch (error) {
         console.log(error);
